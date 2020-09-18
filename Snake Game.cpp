116c2376d36a06f844e657dd603ffb6ad398ff95
Chase Staples <s528345@nwmissouri.edu>
2020-09-17 20:35:29 -0500

    Creating setup and drawing out board
    Starting to draw out the board and make sure everything is printing like it should

--- Snake Game.cpp
+++ Snake Game.cpp
@@ -2,13 +2,51 @@
 using namespace std;
 
 bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum Direction {STOP = 0, LEFT, RIGHT, UP, DOWN};
+Direction direction;
+
+
 void Setup() {
 
     gameOver = false;
+    direction = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+
 }
 
 void Draw() {
+    system("cls"); //system("clear")
+    for(int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
 
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            if(j == 0)
+                cout << "#";
+            if(i == y && j == x)
+                cout << "O";
+            else if(i == fruitY && j == fruitX)
+                cout << "F"
+            else
+                cout << " ";
+            if(j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < width + 2; i++)
+        cout << "#";
+      cout << endl;
 }
 
 void Input(){ 
@@ -26,6 +64,7 @@ int main(){
         Draw();
         Input();
         Logic();
+        //Sleep(10); sleep(10);
     }
     return 0;
 }
